5077aedb456476e408593db3aae88f8da7d639d8
agr3w <suzanakampa.sk@gmail.com>
2026-02-07 21:55:41 -0300

    ✨feat: Adiciona componente LoadingScreen para melhorar a experiência de carregamento nas rotas

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,6 +1,8 @@
 import React, { Suspense, lazy } from "react";
 import { Routes, Route } from "react-router-dom";
 
+import LoadingScreen from "./components/ui/LoadingScreen";
+
 // Importações Lazy
 const Home = lazy(() => import("@pages/public/Home"));
 const About = lazy(() => import("@pages/public/About"));
@@ -17,7 +19,7 @@ export default function Router() {
   return (
     /** Suspense apenas como tapa-burado para carregamento assíncrono,
      mais para frente iremos melhorar isso e colocar um componente de loading específico */
-    <Suspense fallback={<div>Carregando...</div>}>
+    <Suspense fallback={<LoadingScreen />}>
       <Routes>
         {/* Rotas Públicas */}
         <Route path="/" element={<Home />} />
@@ -27,23 +29,64 @@ export default function Router() {
 
         {/* Rotas Admin */}
         <Route path="/admin" element={<AdminLayout />}>
-
           {/* O "index" significa que é a rota padrão /admin */}
-          <Route index element={<Dashboard />} />
+          <Route
+            index
+            element={
+              <Suspense fallback={<LoadingScreen />}>
+                <Dashboard />
+              </Suspense>
+            }
+          />
 
           {/* Admin - Blog (Visão Geral) */}
-          <Route path="blog" element={<BlogManager />} />
+          <Route
+            path="blog"
+            element={
+              <Suspense fallback={<LoadingScreen />}>
+                <BlogManager />
+              </Suspense>
+            }
+          />
 
           {/* Admin - Leads */}
-          <Route path="leads" element={<LeadsManager />} />
+          <Route
+            path="leads"
+            element={
+              <Suspense fallback={<LoadingScreen />}>
+                <LeadsManager />
+              </Suspense>
+            }
+          />
 
           {/* admin - Site Editor */}
-          <Route path="editor" element={<SiteEditor />} />
+          <Route
+            path="editor"
+            element={
+              <Suspense fallback={<LoadingScreen />}>
+                <SiteEditor />
+              </Suspense>
+            }
+          />
         </Route>
 
         {/* Admin - Blog (Editor) */}
-        <Route path="/admin/blog/novo" element={<BlogEditorPage />} />
-        <Route path="/admin/blog/editar/:id" element={<BlogEditorPage />} />
+        <Route
+          path="/admin/blog/novo"
+          element={
+            <Suspense fallback={<LoadingScreen />}>
+              <BlogEditorPage />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/admin/blog/editar/:id"
+          element={
+            <Suspense fallback={<LoadingScreen />}>
+              <BlogEditorPage />
+            </Suspense>
+          }
+        />
       </Routes>
     </Suspense>
   );
